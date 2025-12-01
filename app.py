9c3449befdcb96f86742692a0f78e84cb0057a4e
Manjuatgithub25 <manjunathyashram5@gmail.com>
2025-12-01 14:01:17 +0530

    Update the embeddings and requirements

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama.embeddings import OllamaEmbeddings
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
